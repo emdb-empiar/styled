d82f071f1cf48de9d7cabdd097d62f4e2738ed41
paulkorir <pkorir@ebi.ac.uk>
2019-02-04 16:32:50 +0000

    Major cleanup
    * split styles into module 'assets'
    * now all styles and colors (fg/bg) present
    * StyleError now exists
    * should be 100% unicode internally
    * added classmethods _validate and _clean (Django-inspired)
    * extended tests

---
+++ styled/styled.py
@@ -0,0 +1,158 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function
+
+import re
+from assets import COLOURS, STYLE_NAMES, FG_COLOURS, BG_COLOURS, ESC, END
+
+
+class StyleError(Exception):
+    def __init__(self, *args, **kwargs):
+        super(StyleError, self).__init__(*args, **kwargs)
+
+
+class Styled(str):
+    pattern = re.compile(r".*?(?P<pattern>[[][[].*?[|].*?[]][]]).*?", re.UNICODE)
+    styled_text = re.compile(r".*?[[][[].*?[\"'](?P<text>.*?)[\"'][|](?P<styles>(\w+[:-]?)+).*?[]][]].*", re.UNICODE)
+
+    # todo: make plain and styled readonly attributes that are only set at construction time
+    def __new__(cls, s, *args, **kwargs):
+        if isinstance(s, str):
+            obj = super(Styled, cls).__new__(cls, s.decode('utf-8'))
+        elif isinstance(s, unicode):
+            obj = super(Styled, cls).__new__(cls, s)
+        # format string using args and kwargs
+        cls._plain = obj.format(*args, **kwargs).decode('utf-8')
+        # extract tokens
+        cls._tokens = obj._find_tokens(cls._plain)
+        # validate
+        cls._validate(cls._tokens)
+        # remove duplicates
+        cls._cleaned_tokens = cls._clean(cls._tokens)
+        # transform text with tokens and styles
+        cls._styled = obj._transform(cls._plain, cls._cleaned_tokens)
+        # unstyled version for length inference
+        cls._unstyled = obj._transform(cls._plain, cls._cleaned_tokens, apply=False)
+        return obj
+
+    def __init__(self, s, *args, **kwargs):
+        super(Styled, self).__init__(s)
+
+    @staticmethod
+    def transform(token):
+        """Static method that converts tokens into styled text"""
+        start, end, text, styles = token
+        s = u''
+        for style in styles:
+            pos = None
+            try:
+                pos, style_ = style.split('-')
+            except ValueError:
+                style_ = style
+            try:
+                if pos == u'fg':
+                    s += u'{}[{}m'.format(ESC, FG_COLOURS(style_))
+                elif pos == u'bg':
+                    s += u'{}[{}m'.format(ESC, BG_COLOURS(style_))
+                else:
+                    s += u'{}[{}m'.format(ESC, STYLE_NAMES[style_])
+            except KeyError:
+                raise StyleError("Unknown style '{}'".format(style_))
+        return u'{}{}{}'.format(s, text, END)
+
+    @classmethod
+    def _transform(cls, plain, tokens, apply=True):
+        """Transform the whole string into a styled string"""
+        i = 0
+        styled = u''
+        for token in tokens:
+            start, end, text, styles = token
+            if apply:
+                styled += plain[i:start] + cls.transform(token)
+            else:
+                styled += plain[i:start] + text
+            i = end
+        styled += plain[i:]
+        return styled
+
+    @classmethod
+    def _find_tokens(cls, string):
+        """Find all style tokens in the string"""
+        tokens = list()
+        index = 0
+        pos = 0
+        while True:
+            string = string[index:]
+            pattern = cls.pattern.match(string)
+            if not pattern:  # or not styled_text:
+                break
+            found_pattern = pattern.group(u'pattern')
+            styled_text = cls.styled_text.match(found_pattern)
+            if not styled_text:
+                raise StyleError(u"Invalid tokens in pattern {}".format(found_pattern))
+            text = styled_text.group(u'text')
+            styles = styled_text.group(u'styles').split(u':')
+            t_ = (pattern.start() + pos + (pattern.end() - len(found_pattern)), pattern.end() + pos, text, styles)
+            tokens.append(
+                t_,
+            )
+            index = pattern.end()
+            pos += index
+        return tokens
+
+    @classmethod
+    def _validate(cls, tokens):
+        """Validate styling
+
+        * no multiple fgs or bgs
+        """
+        for start, end, text, styles in tokens:
+            fgs = list()
+            bgs = list()
+            other = list()
+            for style in styles:
+                pos = None
+                try:
+                    pos, style_ = style.split('-')
+                except ValueError:
+                    style_ = style
+                if pos == 'fg':
+                    fgs.append(style)
+                elif pos == 'bg':
+                    bgs.append(style)
+                else:
+                    other.append(style)
+            if len(fgs) > 1:
+                raise ValueError("Multiple foreground styles for text '{}': {}".format(text, ', '.join(styles)))
+            if len(bgs) > 1:
+                raise ValueError("Multiple background styles for text '{}': {}".format(text, ', '.join(styles)))
+
+    @classmethod
+    def _clean(cls, tokens):
+        """Remove duplicates and sundry things"""
+        cleaned_tokens = list()
+        for start, end, text, styles in tokens:
+            cleaned_tokens.append((start, end, text, list(set(styles))))
+        return cleaned_tokens
+
+    def __len__(self):
+        return len(self._unstyled)
+
+    def __str__(self):
+        return self._styled.encode('utf-8')
+
+    def __eq__(self, other):
+        return self._unstyled.encode('utf-8') == other
+
+    def __add__(self, other):
+        """styled + other"""
+        if isinstance(other, Styled):
+            return Styled(self._plain + other._plain)
+        else:
+            return Styled(self._plain + other)
+
+    def __radd__(self, other):
+        """other + styled"""
+        if isinstance(other, Styled):
+            return Styled(other._plain + self._plain)
+        else:
+            return Styled(other + self._plain)
--- styled/tests.py
+++ styled/tests.py
@@ -1,70 +1,115 @@
+# -*- coding: utf-8 -*-
 from __future__ import print_function
 from unittest import TestCase
 
-import core
+from styled import Styled, StyleError
 
 
 class TestStyled(TestCase):
-    # def test_default(self):
-    #     string = 'this is a string'
-    #     S = core.Styled(string)
-    #     self.assertEqual(S, string)
-    #     self.assertIsInstance(S, core.Styled)
-    #
-    # def test_format(self):
-    #     string = 'I have {} {thing}s.'
-    #     pos = 5
-    #     thing = 'orange'
-    #     S = core.Styled(string, pos, thing=thing)
-    #     print(S)
-    #     self.assertEqual(S, string.format(pos, thing=thing))
-    #
-    # def test_length(self):
-    #     string = 'I have {} {thing}s.'
-    #     pos = 11
-    #     thing = 'pencil'
-    #     S = core.Styled(string, pos, thing=thing)
-    #     self.assertEqual(len(S), len(string.format(pos, thing=thing)))
+    def test_default(self):
+        string = 'this is a string'
+        S = Styled(string)
+        self.assertEqual(S, string)
+        self.assertIsInstance(S, Styled)
 
-    def test_style(self):
-        ss1 = """[[ 'Red'|red:blink ]]"""
-        ss2 = """ roses are usually [[ 'red'|red:bold ]]. boogey oogey"""
-        # S1 = core.Styled(ss1)
-        # print(S1)
-        # print(S1.finds)
-        # print()
-        # S2 = core.Styled(ss2)
-        # print(S2)
-        # print(S2.finds)
-        # print()
-        S3 = core.Styled(ss1 + ss2 + ss2 + ss1 + ss1 + ss2)
-        # print(S3)
-        # print(S3.finds)
-        # print()
-        ss3 = "Note that [[ 'm.start(group)'|bold:green ]] will equal [[ 'm.end(group)'|bold:yellow ]] if [[ 'group'|underlined ]] " \
-              "matched a null string. For example, after [[ 'm = re.search('b(c?)', 'cba')'|bold:blue ]], " \
-              "[[ 'm.start(0)'|bold ]] is 1, [[ 'm.end(0)'|bold ]] is 2, [[ 'm.start(1)'|bold ]] and " \
-              "[[ 'm.end(1)'|bold ]] are both 2, and [[ 'm.start(2)'|bold ]] raises an [[ 'IndexError'|underlined ]] " \
-              "exception."
-        S4 = core.Styled(ss3)
-        # print(S4)
-        # print(S4.finds)
-        # print()
-        ss4 = "This text [[ 'blinks'|reverse:magenta ]]!"
-        S5 = core.Styled(ss4)
+    # def test_style(self):
+    #     ss1 = """[[ 'Red'|red:blink ]]"""
+    #     ss2 = """ roses are usually [[ 'red'|red:bold ]]. boogey oogey"""
+    #     # S1 = Styled(ss1)
+    #     # print(S1)
+    #     # print(S1.finds)
+    #     # print()
+    #     # S2 = Styled(ss2)
+    #     # print(S2)
+    #     # print(S2.finds)
+    #     # print()
+    #     S3 = Styled(ss1 + ss2 + ss2 + ss1 + ss1 + ss2)
+    #     # print(S3)
+    #     # print(S3.finds)
+    #     # print()
+    #     ss3 = "Note that [[ 'm.start(group)'|bold:green ]] will equal [[ 'm.end(group)'|bold:yellow ]] if [[ 'group'|underlined ]] " \
+    #           "matched a null string. For example, after [[ 'm = re.search('b(c?)', 'cba')'|bold:blue ]], " \
+    #           "[[ 'm.start(0)'|bold ]] is 1, [[ 'm.end(0)'|bold ]] is 2, [[ 'm.start(1)'|bold ]] and " \
+    #           "[[ 'm.end(1)'|bold ]] are both 2, and [[ 'm.start(2)'|bold ]] raises an [[ 'IndexError'|underlined ]] " \
+    #           "exception."
+    #     print(ss3)
+    #     S4 = Styled(ss3)
+    #     print(S4)
+    #     # print(S4.finds)
+    #     # print()
+    #     ss4 = "This text [[ 'blinks'|reverse:magenta ]]!"
+    #     S5 = Styled(ss4)
+    #     print(S5)
 
     def test_find_tokens(self):
-        s = """[[ 'a word'|red ]]"""
-        s = core.Styled(s)
-        self.assertItemsEqual(s._tokens, [(0, 18, 'a word', ['red'])])
-        s = core.Styled("""[[ 'your {} is open'|blue ]]""", 'bank')
-        self.assertItemsEqual(s._tokens, [(0, 30, 'your bank is open', ['blue'])])
+        s = """[[ 'a word'|fg-red ]]"""
+        s = Styled(s)
+        self.assertItemsEqual(s._tokens, [(0, 21, u'a word', [u'fg-red'])])
+        s = Styled("""[[ 'your {} is open'|fg-blue ]]""", 'bank')
+        self.assertItemsEqual(s._tokens, [(0, 33, u'your bank is open', [u'fg-blue'])])
 
     def test_length(self):
         u = 'I am the most handsome guy in the room.'
-        s = core.Styled("I am the most [[ 'handsome'|bold ]] guy in the room.")
-        print(s)
-        print(u)
-        print(s._plain)
-        print(s._unstyled)
+        u_list = u.split(' ')
+        s = Styled(" ".join(u_list[:4] + ["[[ '{}'|bold ]]".format(u_list[4])] + u_list[5:]))
         self.assertEqual(len(u), len(s))
+
+    def test_format(self):
+        u = "This is a very {} affair in which {count} people were involved."
+        s = Styled(u, 'noble', count=38)
+        self.assertEqual(s, u.format('noble', count=38))
+
+    def test_quotes(self):
+        sq = Styled("I have a [[ 'bold \"hair\"'|fg-red ]] face.")
+        dq = Styled('I have a [[ "bold \'hair\'"|fg-red ]] face.')
+        tq = Styled("""I have a [[ "bold 'hair'"|fg-red ]] face.""")
+        self.assertItemsEqual(sq._tokens, [(9, 35, u"bold 'hair'", [u'fg-red'])])
+        self.assertItemsEqual(dq._tokens, [(9, 35, u"bold 'hair'", [u'fg-red'])])
+        self.assertItemsEqual(tq._tokens, [(9, 35, u"bold 'hair'", [u'fg-red'])])
+
+    def test_style_error(self):
+        with self.assertRaises(StyleError):
+            Styled('I have a [[ "bold\'hair|fg-red ]] face')
+
+    def test_concatenate(self):
+        s1 = Styled("This is the [[ 'end'|bold ]]!")
+        u1 = " And I will be finished."
+        c1 = s1 + u1
+        self.assertEqual(c1, s1 + u1)
+        self.assertIsInstance(c1, Styled)
+        u2 = "I will be finished..."
+        s2 = Styled(" when this [[ 'ends'|bold ]]!")
+        c2 = u2 + s2
+        self.assertEqual(c2, u2 + s2)
+        self.assertIsInstance(c2, Styled)
+
+    def test_fg_colour(self):
+        s = Styled("I have never seen a [[ 'white'|fg-white:bold ]] stallion.")
+        self.assertIsInstance(s, Styled)
+
+    def test_bg_colour(self):
+        s = Styled("[[ 'White bold text on a black background.'|fg-black:bg-white:bold ]]")
+        self.assertIsInstance(s, Styled)
+
+    def test_catch_multiple_fgs(self):
+        with self.assertRaises(ValueError):
+            s = Styled("[[ 'useless'|fg-red:fg-orange ]]")
+
+    def test_catch_multiple_bgs(self):
+        with self.assertRaises(ValueError):
+            s = Styled("[[ 'useless'|bg-red:bg-orange ]]")
+
+    def test_clean_tokens(self):
+        s = Styled("[[ 'some text'|bold:bold ]]")
+        self.assertItemsEqual(s._cleaned_tokens, [(0, 27, u"some text", [u'bold'])])
+
+    def test_iteration(self):
+        s = Styled("There are some folks who [[ 'gasp'|fg-black:bg-deep_sky_blue_2:underlined:blink ]] at the thought "
+                   "of the military. ")
+        s += 'Woe unto them!'
+        self.assertIsInstance(s, Styled)
+        print(s)
+
+
+
+
